a476c65c0c28a8f4e2dc886366b5999becc608c0
aeamosov <a.e.amosov@gmail.com>
2024-02-09 16:00:50 +0300

    GI

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Александр\GitHub\Blackjack-learning-demo\venv
+set VIRTUAL_ENV=C:\Users\amosov\Documents\GitHub\Blackjack-learning-demo\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

8349caf0333b71ab95fae31c89547f694d82f6b0
aeamosov <a.e.amosov@gmail.com>
2024-03-03 11:37:24 +0300

    Card draw -remade

--- app.py
+++ app.py
@@ -6,29 +6,32 @@ import json
 app = Flask(__name__)
 
 # List to store drawn cards
-drawn_cards = []
+hand = []
 def create_deck():
-    deck_id = json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
-    return deck_id
+	deck_id = json.loads(requests.post('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1').text)['deck_id']
+	return deck_id
 
 def draw_card(deck_id):
-    card = json.loads(requests.post('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=1').text)
-    return card
+	card = json.loads(requests.post('https://deckofcardsapi.com/api/deck/'+deck_id+'/draw/?count=1').text)
+	return card
 	
 @app.route('/')
 def index():
-    return render_template('index.html', drawn_cards=drawn_cards)
+	return render_template('index.html', hand=hand)
 
-@app.route('/draw')
-def draw():
-    deck_id = create_deck()
-    card = draw_card(deck_id)['cards'][0]
-    card_image = card['image']
-    
-    # Append the drawn card to the list
-    drawn_cards.append(card_image)
-    
-    return render_template('index.html', card_image=card_image, drawn_cards=drawn_cards)
+@app.route('/game')
+def new_game():
+	deck_id = create_deck()
+	global hand 
+	hand = []	
+	return render_template('game.html', deck_id=deck_id, hand=hand)
+	
+@app.route('/game/<deck_id>/draw')
+def draw(deck_id):
+	card = draw_card(deck_id)['cards'][0]	
+	# Append the drawn card_image to the hand
+	hand.append(card)
+	return render_template('game.html', deck_id=deck_id, hand=hand)
 
 #Конфиг запуска	
 if __name__=='__main__':
